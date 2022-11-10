ac8735ca0f18a1175df171639a504da03ed09f1a
Gabriel Guerrero <gabi.guerrero94@mail.com>
2022-11-10 16:28:45 -0300

    se agrego archivo login.jv y home.html (redireccionamiento de html a html

---
+++ login.js
@@ -0,0 +1,17 @@
+function validate () {
+    let usuario = document.getElementById("usuario").value;
+    let clave = document.getElementById("clave").value;
+    if (usuario = "usuario" && clave == "1234") {
+    
+        window.location.href = "home.html";
+        return false;
+    } else {
+        alert("usuario o clave incorrectos");
+    }
+}
+let button = document.getElemntById("button");
+button. addEventListener("click", (e)=> {
+      e.preventDeFault();
+    validate();
+}) 
+  
